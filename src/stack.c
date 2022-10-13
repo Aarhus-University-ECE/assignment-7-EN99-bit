217cb6c460604258389047753779961fe3fa9925
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-10-13 15:55:33 +0200

    Afl. 1

--- src/stack.c
+++ src/stack.c
@@ -1,25 +1,35 @@
 #include "stack.h"
+#include <assert.h>
 
 void initialize(stack* s){
   //implement initialize here
+  s->head = NULL;
 }
 
 void push(int x, stack* s){
-    //implement push here
+    node *element = malloc(sizeof(node));
+    element->next = s->head;
+    element->data = x;
+    s->head = element;
 }
 
 int pop(stack* s){
-    // implement pop here
-  return -1;
+assert(s->head != NULL);
+int Pop_Element = s->head->data;
+s->head = s->head->next;
+  return Pop_Element;
 }
 
 bool empty(stack* s)
 {
-  //implement empty here
-  return false;
+  if (s->head = NULL)
+    return true;
+  else
+    return false;
 }
 
 bool full(stack* s) {
-    //implement full here
+    //Vi kan ikke adde data oven i en stack der har status som "full". Det ville give overflow fejl. 
+    //Derfor bliver jeg nødt til at return false
   return false;
 }
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,39 @@
 #include "taylor_sine.h"
+#include <stdlib.h>
+#include <math.h>
+
+double factorial (int n){
+    if (n<=1){
+        return 1;  
+    }
+    else
+        return (n*factorial(n-1));
+             
+}
+
 double taylor_sine(double x, int n)
 {
-    /* implement your function here */
-    
-    return -1.0;
+double sin = x;
+double square_x = x*x;
+double number = x; //tæller
+double num=1; //initialising numerator
+int q = 1;
+
+
+for (int i=1; i<=n; i++){
+q = q+2;
+number = number*square_x;
+num = factorial(q);
+
+if (i % 2 == 0)//vi tjekker om tallet er lige eller ulige ift. om vi skal trække fra eller lægge til. Anvender modulus
+{
+    sin+=number/num;
+} 
+else
+{
+sin-=number/num;
+}
+
+}
+    return sin;
 }
